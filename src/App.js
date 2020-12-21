f26523578fa68c009d95e7b897cab4e34e2571df
Kothari <shreya.kothari@accenture.com>
2020-12-21 11:46:02 +0700

    first commits

---
+++ src/App.js
@@ -0,0 +1,33 @@
+
+import './App.css';
+import React, { Component } from "react";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import "bootstrap/dist/css/bootstrap.min.css";
+import ListEmployeeComponent from './components/ListEmployeeComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
+import CreateEmployeeComponent from './components/CreateEmployeeComponent';
+import ViewEmployeeComponent from './components/ViewEmployeeComponent';
+
+
+function App() {
+  return (
+    <div>
+        <Router>
+              <HeaderComponent />
+                <div className="container">
+                    <Switch> 
+                          <Route path = "/" exact component = {ListEmployeeComponent}></Route>
+                          <Route path = "/employees" component = {ListEmployeeComponent}></Route>
+                          <Route path = "/add-employee/:id" component = {CreateEmployeeComponent}></Route>
+                          <Route path = "/view-employee/:id" component = {ViewEmployeeComponent}></Route>
+                          {/* <Route path = "/update-employee/:id" component = {UpdateEmployeeComponent}></Route> */}
+                    </Switch>
+                </div>
+              <FooterComponent />
+        </Router>
+    </div>
+    
+  );
+}
+export default App;
